24f016701c4912bb9b5942283586e890f30b95b1
isaacBejarano <isaacbejaranopro@gmail.com>
2020-09-18 13:43:32 +0200

    FASE 1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,26 @@
 "use strict";
-function createCar(plate, brand, color) {
+/* GLOBALS */
+var cars = []; // Cars collection
+// DOM REFS
+var formCreateCar = document.querySelector("form"); // (!) =>  not null && HTMLFormElement
+var inputPlate = document.querySelector('[name="input_plate"]');
+var inputBrand = document.querySelector('[name="input_brand"]');
+var inputColor = document.querySelector('[name="input_color"]');
+/* EVENTS */
+formCreateCar.addEventListener("submit", function (e) {
+    createCar(e, inputPlate.value, inputBrand.value, inputColor.value);
+});
+/* AUX */
+function createCar(e, plate, brand, color) {
+    var carInfo = document.getElementById("carInfo");
     var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
+    car.addWheel(new Wheel(16, "Firestone"));
+    // car of cars
+    cars.push(car);
+    // toString
+    carInfo.innerHTML = "\n\t\t<i class=\"fas fa-car\"></i>\n\t\tCAR: <span class=\"text-dark\">" + (cars.indexOf(car) + 1) + "</span> </br>\n\t\t<i class=\"far fa-address-card\"></i>\n\t\tPLATE: <span class=\"text-dark\">" + car.plate + "</span> </br>\n\t\t<i class=\"fas fa-palette\"></i>\n\t\tCOLOR: <span class=\"text-dark\">" + car.color + "</span> </br>\n\t\t<i class=\"fas fa-signature\"></i>\n\t\tBRAND: <span class=\"text-dark\">" + car.brand + "</span> </br>\n\t\t<i class=\"fas fa-dot-circle\"></i>\n\t\tWHEELS: <span class=\"text-dark\">\n\t\t\t" + +car.wheels[car.wheels.length - 1].diameter + " inches.\n\t\t\t" + car.wheels[car.wheels.length - 1].brand + "\t\t\t\n\t\t\t</span> </br>\n\t";
+    carInfo.classList.remove("is-none");
+    // prevent submit
+    e.preventDefault();
+    e.stopPropagation();
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,46 @@
+/* GLOBALS */
+let cars: Car[] = []; // Cars collection
 
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+// DOM REFS
+const formCreateCar = document.querySelector("form") as HTMLFormElement; // (!) =>  not null && HTMLFormElement
+const inputPlate = document.querySelector('[name="input_plate"]') as HTMLFormElement;
+const inputBrand = document.querySelector('[name="input_brand"]') as HTMLFormElement;
+const inputColor = document.querySelector('[name="input_color"]') as HTMLFormElement;
+
+/* EVENTS */
+formCreateCar.addEventListener("submit", function (e) {
+	createCar(e, inputPlate.value, inputBrand.value, inputColor.value);
+});
+
+/* AUX */
+function createCar(e: Event, plate: string, brand: string, color: string) {
+	const carInfo = document.getElementById("carInfo") as HTMLDivElement;
+	const car = new Car(plate, color, brand);
+	car.addWheel(new Wheel(16, "Firestone"));
+
+	// car of cars
+	cars.push(car);
+
+	// toString
+	carInfo.innerHTML = `
+		<i class="fas fa-car"></i>
+		CAR: <span class="text-dark">${cars.indexOf(car) + 1}</span> </br>
+		<i class="far fa-address-card"></i>
+		PLATE: <span class="text-dark">${car.plate}</span> </br>
+		<i class="fas fa-palette"></i>
+		COLOR: <span class="text-dark">${car.color}</span> </br>
+		<i class="fas fa-signature"></i>
+		BRAND: <span class="text-dark">${car.brand}</span> </br>
+		<i class="fas fa-dot-circle"></i>
+		WHEELS: <span class="text-dark">
+			${+car.wheels[car.wheels.length - 1].diameter} inches.
+			${car.wheels[car.wheels.length - 1].brand}			
+			</span> </br>
+	`;
+
+	carInfo.classList.remove("is-none");
+
+	// prevent submit
+	e.preventDefault();
+	e.stopPropagation();
 }
-
