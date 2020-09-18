41542357e137bd9f198abf2ff7d08949a65b1019
isaacBejarano <isaacbejaranopro@gmail.com>
2020-09-18 23:56:01 +0200

    FASE 1.4

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,6 +5,7 @@ var cars = []; // Cars collection
 var Forms;
 (function (Forms) {
     Forms[Forms["form_create_car"] = 0] = "form_create_car";
+    Forms[Forms["form_add_wheels"] = 1] = "form_add_wheels";
 })(Forms || (Forms = {}));
 var Elements;
 (function (Elements) {
@@ -12,17 +13,24 @@ var Elements;
     Elements[Elements["input_brand"] = 1] = "input_brand";
     Elements[Elements["input_color"] = 2] = "input_color";
 })(Elements || (Elements = {}));
+var carInfo = document.getElementById("carInfo");
 var formCreateCar = document.forms[Forms.form_create_car];
+var formAddWheels = document.forms[Forms.form_add_wheels];
 var inputPlate = formCreateCar.elements[Elements.input_plate];
 var inputBrand = formCreateCar.elements[Elements.input_brand];
 var inputColor = formCreateCar.elements[Elements.input_color];
 // EVENTS
 formCreateCar.addEventListener("submit", function (e) {
-    createCar(e, inputPlate.value, inputBrand.value, inputColor.value);
+    createCar(inputPlate.value, inputBrand.value, inputColor.value);
+    formPreventAndReset(e, this);
+});
+formAddWheels.addEventListener("submit", function (e) {
+    formCreateCar.classList.remove("is-none"); // CSS
+    formAddWheels.classList.add("is-none"); // CSS
+    formPreventAndReset(e, this);
 });
 // AUX
-function createCar(e, plate, brand, color) {
-    var carInfo = document.getElementById("carInfo");
+function createCar(plate, brand, color) {
     var carInfoSpans = document.querySelectorAll("#carInfo p span");
     var car = new Car(plate, color, brand);
     car.addWheel(new Wheel(16, "Firestone"));
@@ -33,8 +41,15 @@ function createCar(e, plate, brand, color) {
     carInfoSpans[2].textContent = car.brand;
     carInfoSpans[3].textContent = car.color;
     carInfoSpans[4].textContent = "\n\t\t" + +car.wheels[car.wheels.length - 1].diameter + " inches.\n\t\t" + car.wheels[car.wheels.length - 1].brand;
-    carInfo.classList.remove("is-none"); // CSS overwrite
+    // CSS
+    carInfo.classList.remove("is-none");
+    formCreateCar.classList.add("is-none");
+    formAddWheels.classList.remove("is-none");
+}
+function formPreventAndReset(e, ref) {
     // prevent submit
     e.preventDefault();
     e.stopPropagation();
+    // clear input
+    ref.reset();
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -4,27 +4,36 @@ let cars: Car[] = []; // Cars collection
 // REFS
 enum Forms {
 	"form_create_car" = 0,
+	"form_add_wheels" = 1,
 }
 enum Elements {
 	"input_plate" = 0,
 	"input_brand" = 1,
 	"input_color" = 2,
 }
+const carInfo = document.getElementById("carInfo") as HTMLElement;
 const formCreateCar = document.forms[Forms.form_create_car] as HTMLFormElement;
+const formAddWheels = document.forms[Forms.form_add_wheels] as HTMLFormElement;
 const inputPlate = formCreateCar.elements[Elements.input_plate] as HTMLInputElement;
 const inputBrand = formCreateCar.elements[Elements.input_brand] as HTMLInputElement;
 const inputColor = formCreateCar.elements[Elements.input_color] as HTMLInputElement;
 
 // EVENTS
 formCreateCar.addEventListener("submit", function (e) {
-	createCar(e, inputPlate.value, inputBrand.value, inputColor.value);
+	createCar(inputPlate.value, inputBrand.value, inputColor.value);
+	formPreventAndReset(e, this);
+});
+formAddWheels.addEventListener("submit", function (e) {
+	formCreateCar.classList.remove("is-none"); // CSS
+	formAddWheels.classList.add("is-none"); // CSS
+	formPreventAndReset(e, this);
 });
 
 // AUX
-function createCar(e: Event, plate: string, brand: string, color: string) {
-	const carInfo = document.getElementById("carInfo") as HTMLElement;
+function createCar(plate: string, brand: string, color: string) {
 	const carInfoSpans = document.querySelectorAll("#carInfo p span") as NodeListOf<HTMLSpanElement>;
 	const car = new Car(plate, color, brand);
+
 	car.addWheel(new Wheel(16, "Firestone"));
 
 	cars.push(car); // car of cars
@@ -38,9 +47,17 @@ function createCar(e: Event, plate: string, brand: string, color: string) {
 		${+car.wheels[car.wheels.length - 1].diameter} inches.
 		${car.wheels[car.wheels.length - 1].brand}`;
 
-	carInfo.classList.remove("is-none"); // CSS overwrite
+	// CSS
+	carInfo.classList.remove("is-none");
+	formCreateCar.classList.add("is-none");
+	formAddWheels.classList.remove("is-none");
+}
 
+function formPreventAndReset(e: Event, ref: HTMLFormElement) {
 	// prevent submit
 	e.preventDefault();
 	e.stopPropagation();
+
+	// clear input
+	ref.reset();
 }
